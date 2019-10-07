046765d97d1891c5b32b42156a38a0e2efa47463
Elan Ruusamäe <glen@pld-linux.org>
2019-10-07 20:56:11 +0300

    Setup Pimple container

--- src/Application.php
+++ src/Application.php
@@ -2,8 +2,40 @@
 
 namespace glen\PimpleConsoleApplication;
 
+use Pimple\Container;
 use Symfony\Component\Console\Application as BaseApplication;
 
 class Application extends BaseApplication
 {
+    /** @var Container */
+    protected $container;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->container = $this->createContainer();
+    }
+
+    /**
+     * @return Container
+     */
+    protected function createContainer()
+    {
+        $app = new Container();
+        $this->registerProviders($app);
+
+        return $app;
+    }
+
+    protected function registerProviders(Container $app)
+    {
+    }
+
+    /**
+     * @return Container
+     */
+    public function getContainer()
+    {
+        return $this->container;
+    }
 }
